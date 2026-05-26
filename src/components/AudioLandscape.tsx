7f475aafbd75c0c72af3202c97e550d60326b068
roman krasilnikov <krasilnikovroman5@gmail.com>
2026-05-26 23:22:27 +0700

    github pages deploy

--- src/components/AudioLandscape.tsx
+++ src/components/AudioLandscape.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import  { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
